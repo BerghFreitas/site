f97f08a146086aff78085a117febd3b5de08b1ae
BerghFreitas <berg2843@gmail.com>
2024-02-21 23:35:49 -0300

    referenciando
    foi referenciado no js e implementado no projeto, a selecao de massa, molho e quantidade

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
-const quantidadeIngredientes = []
+
 const frm = document.querySelector("form")
+const tipoMolho = document.querySelector('#tipoMolho')
+const tipoMassa = document.querySelector('#tipoMassa')
 const resp1 = document.querySelector('#outRespPedido')
 const resp2 = document.querySelector('#outRespErro')
+const ingredientesList = document.querySelector(`#ingredientes`)
+
+
 
 frm.addEventListener  ('submit',  (e) => {
-
+    const molho = frm.saborMolho.value
+    const massa = frm.tipoMassa.value
+    const quantidadeIngredientes = frm.quantidadeIngredientes.value [0]  
+    
+    if (quantidadeIngredientes == 3) {
+        resp1.innerText = `vc escolheu ${molho}, macarrão tipo ${massa} e 3 ingredientes `
+        resp1.style.color = "blue"
+    } else {
+        resp1.innerText = `vc escolheu ${molho}, macarrão tipo ${massa} e 6 ingredientes`
+        resp1.style.color = "green"
+    }
+    e.preventDefault()
 })
+
+
+
+/**/
