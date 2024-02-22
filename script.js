4d204a5df8bdc12ae295268ab4ce53ab29bf3787
BerghFreitas <berg2843@gmail.com>
2024-02-21 21:43:35 -0300

    modificacao
    foi excluida um monte de coisa e colocado em pratica alguma boa praticas de codigo para dar uma melhorada na sintese

--- script.js
+++ script.js
@@ -1,42 +1,8 @@
-var quantidadeIngredientes = 6;
+const quantidadeIngredientes = []
+const frm = document.querySelector("form")
+const resp1 = document.querySelector('#outRespPedido')
+const resp2 = document.querySelector('#outRespErro')
 
-function alterarQuantidadeIngredientes() {
-  quantidadeIngredientes = parseInt(document.getElementById("quantidade-ingredientes").value);
+frm.addEventListener  ('submit',  (e) => {
 
-  // Limpa a lista de ingredientes ao alterar a quantidade
-  document.getElementById("ingredientes").innerHTML = "";
-
-  // Exibe a lista completa de 6 ingredientes
-  for (var i = 1; i <= 6; i++) {
-    var label = document.createElement("label");
-    label.innerHTML = `<input type="checkbox" name="ingredient" value="ingrediente${i}"> Ingrediente ${i}<br>`;
-    document.getElementById("ingredientes").appendChild(label);
-  }
-}
-
-function fazerPedido() {
-  var sabor = document.getElementById("sabor").value;
-  var ingredientes = document.getElementsByName("ingredient");
-  var tipo = document.getElementById("tipo")
-  var ingredientesSelecionados = [];
-
-  var ingredientesSelecionadosCount = 0;
-  for (var i = 0; i < ingredientes.length; i++) {
-    if (ingredientes[i].checked) {
-      ingredientesSelecionadosCount++;
-      ingredientesSelecionados.push(ingredientes[i].value);
-    }
-  }
-
-  if (ingredientesSelecionadosCount !== quantidadeIngredientes) {
-    document.getElementById("erro").innerHTML = "Selecione exatamente " + quantidadeIngredientes + " ingredientes.";
-    return;
-  }
-
-  var pedido = "Sabor: " + sabor + "<br>Ingredientes: " + ingredientesSelecionados.join(", ");
-  document.getElementById("pedido").innerHTML = pedido;
-  document.getElementById("erro").innerHTML = "";
-}
-
-// Inicialmente, cria a lista de ingredientes com base na quantidade padrão
-alterarQuantidadeIngredientes();
+})
